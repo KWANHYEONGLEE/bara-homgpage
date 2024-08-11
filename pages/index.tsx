0b11fcd851f3c4471a605036ab00ef3705c0ca92
lucky-jo <last2599@gmail.com>
2024-08-11 14:08:44 +0900

    feat: 파트너 페이지 개편

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from 'components/common/Layout'
 const IndexPage = () => (
   <Layout title="심바" description="심바를 만나면 육아에도 정답이 있다!">
     <img src="/bara-info.png" alt="bara-info" style={{ width: '100%' }} />
-    <img src="/simba-join.png" alt="bara-info" style={{ width: '100%' }} />
+    <img src="/simba-join.png" alt="bara-join" style={{ width: '100%' }} />
 
     {/* <Hero /> */}
     {/* <InfoSection /> */}
--- pages/partner.tsx
+++ pages/partner.tsx
@@ -2,8 +2,8 @@ import Layout from 'components/common/Layout'
 
 const IndexPage = () => (
   <Layout title="심바" description="심바를 만나면 육아에도 정답이 있다!">
-    <img src="/partner-info.png" alt="bara-info" style={{ width: '100%' }} />
-    <img src="/partner-join.png" alt="bara-info" style={{ width: '100%' }} />
+    <img src="/partner-info.png" alt="partne-info" style={{ width: '100%' }} />
+    <img src="/partner-join.png" alt="partne-join" style={{ width: '100%' }} />
   </Layout>
 )
 
