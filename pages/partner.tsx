cf79398e706e250ed3fdd51911305b24522c16fc
lucky-jo <last2599@gmail.com>
2024-08-11 14:07:28 +0900

    feat: 파트너 페이지 개편

--- pages/partner.tsx
+++ pages/partner.tsx
@@ -1,18 +1,10 @@
-import Layout from "components/common/Layout";
-import Hero from "components/common/Hero";
-import Recruit from "components/page/partner/Recruit";
-import Intro from "components/page/partner/Intro";
-import Banner from "components/page/partner/Banner";
-import History from "components/page/partner/History";
+import Layout from 'components/common/Layout'
 
 const IndexPage = () => (
   <Layout title="심바" description="심바를 만나면 육아에도 정답이 있다!">
-    <Hero />
-    <Recruit />
-    <Intro />
-    <Banner />
-    <History />
+    <img src="/partner-info.png" alt="bara-info" style={{ width: '100%' }} />
+    <img src="/partner-join.png" alt="bara-info" style={{ width: '100%' }} />
   </Layout>
-);
+)
 
-export default IndexPage;
+export default IndexPage
