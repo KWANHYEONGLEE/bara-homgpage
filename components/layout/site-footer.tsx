172cb031eae63459b77466ddcf8cf319c28fdab6
KWANHYEONGLEE <rhksguda@naver.com>
2026-08-12 09:37:06 +0900

    회사소개

--- components/layout/site-footer.tsx
+++ components/layout/site-footer.tsx
@@ -2,11 +2,8 @@ import { companyInfo } from "@/lib/site";
 
 export function SiteFooter() {
   return (
-    // 본문 열과 좌측 기준선을 맞추려면 Section 과 같이
-    // 패딩은 바깥, max-width 는 안쪽에 둬야 한다.
     <footer className="px-5 py-16 lg:px-8 lg:py-24">
       <div className="mx-auto max-w-6xl">
-        {/* 크기를 직접 박으면 타입 스케일을 올려도 여기만 안 커진다 */}
         <address className="t-body-sm border-t border-border pt-8 not-italic leading-[1.9]">
           <p>{companyInfo.legalName}</p>
           <p>
